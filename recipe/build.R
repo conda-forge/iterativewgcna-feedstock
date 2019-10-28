87cb2a18c3029542bca63419eff2c412d06b1ad9
thompsonsed <thompsonsed@gmail.com>
2019-10-28 10:38:35 +0000

    Ensures BiocManager also installs dependencies

--- recipe/build.R
+++ recipe/build.R
@@ -1,3 +1,3 @@
 install.packages("BiocManager", dependencies=TRUE, INSTALL_opts=c("--no-lock"), repos="http://cran.us.r-project.org")
-BiocManager::install(c("impute", "preprocessCore", "GO.db", "AnnotationDbi"))
+BiocManager::install(c("impute", "preprocessCore", "GO.db", "AnnotationDbi"), dependencies=TRUE, INSTALL_opts=c("--no-lock"))
 install.packages("WGCNA", dependencies=TRUE, INSTALL_opts=c("--no-lock"), repos="http://cran.us.r-project.org")
